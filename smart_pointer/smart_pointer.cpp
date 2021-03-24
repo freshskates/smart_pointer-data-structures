d5c640edc91ef5c5456e33770be3113de72d8d8a
freshskates <business.robertcacho@gmail.com>
2021-03-23 20:54:56 -0700

    reverse linked

--- smart_pointer/smart_pointer.cpp
+++ smart_pointer/smart_pointer.cpp
@@ -1,40 +1,65 @@
 #include <iostream>
 using namespace std; 
 
-class Node
-{
+class Node {
 public: 
     shared_ptr<Node> next;
     string data;
     Node() {};
-    Node(string data) : data{ data }, next{ nullptr } {}
+    Node(string&& data) : data{ data }, next{ nullptr } {}
+    Node(string& data) : data{ data}, next{ nullptr } {}
     
-    void setNext(shared_ptr<Node> next) {
-        this->next = move(next);
-    }
-
-    void printData() {
-        cout << this->next << endl; 
-    }
 };
-
+    
 class List {
-public:
+    int nodes = 0; 
     shared_ptr<Node> root;
+    shared_ptr<Node> tail;
+public:
+    List() : root{ nullptr }, tail{ nullptr }{}
 
-    void add(string data) {
-        shared_ptr<Node>temp{ make_shared<Node>(data) };
-        temp->setNext(root);
-        this->root = temp; 
+    void shift(string&& data) {
+        shared_ptr<Node>temp{ make_shared<Node>(move(data)) };
+        temp->next = root;
+        root = temp;
+        nodes++;
     }
 
+    void push(string&& data) {
+        shared_ptr<Node>temp{ make_shared<Node>(move(data)) };
+        if (tail == nullptr || root == nullptr)
+            root = (tail = temp);
+        else {
+            tail->next = temp;
+            tail = temp; 
+        }
+        nodes++;
+    }
+
+    int length() {
+        return nodes;
+    }
+    
     void print() {
-        auto current = this->root;
+        shared_ptr<Node> current = this->root;
         while (current != nullptr) {
             cout << current->data << " -> ";
             current = current->next;
         }
         cout << "null" << endl;
+    }
+
+    void reverse() {
+        shared_ptr<Node> current = this->root, previous = nullptr, next = nullptr;
+        tail = current; 
+        while (current != nullptr)
+        {
+            next = current->next;
+            current->next = previous;
+            previous = current; 
+            current = next; 
+        }
+        root = previous; 
     }
 
 };
@@ -44,22 +69,23 @@ public:
 int main()
 {
  
-    //shared_ptr<Node> bro0{ make_shared<Node>("testing0") };
-    //shared_ptr<Node> bro1{ make_shared<Node>("testing1") };
-    //shared_ptr<Node> bro2{ make_shared<Node>("testing2") };
-    //shared_ptr<Node> bro3{ make_shared<Node>("testing3") };
+    List list0; 
+    list0.push("testing");
+    list0.push("testing1");
+    list0.push("testing2");
+    list0.push("testing3");
+    list0.push("testing4");
+    list0.shift("testing69");
+    list0.print();
+    list0.reverse();
+    list0.print();
+    list0.print();
+    list0.print();
+    list0.reverse();
+    list0.print();
+    list0.print();
 
-    //bro0->setNext(bro1);
-    //bro1->setNext(bro2);
-    //bro2->setNext(bro3);
-    //cout << bro0->data;
-    List l0; 
-    l0.add("1");
-    l0.add("2");
-    l0.add("3");
-    l0.add("4");
-    l0.add("5");
-    l0.print();
+    cout << list0.length() << endl;
 
 }
 
